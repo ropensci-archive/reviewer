0ca91b50707027f63be9a1c135437ccecefcac2e
Amy Stringer <amy.stringer@connect.qut.edu.au>
2018-11-22 14:19:49 +1100

    updated to check if addon was already applied

--- R/enable_html_annotation.R
+++ R/enable_html_annotation.R
@@ -5,7 +5,12 @@
 #'
 #' @export
 enable_html_annotation <- function() {
-  rstudioapi::insertText(Inf,
-    '<script src="https://hypothes.is/embed.js" async></script>'
+  content <- getActiveDocumentContext()
+  if (!any(grepl('<script src="https://hypothes.is/embed.js" async></script>', content$contents))){
+    rstudioapi::insertText(Inf,
+      '<script src="https://hypothes.is/embed.js" async></script>'
   )
+  } else {
+    message("Time for a coffee, you hit the button twice. ")
+  }
 }
